f1dc0be880a4c42a5e85fc4a1b450a69ab23aed0
Prem Kumar Chaudhary <premkumarchaudhary@xe-ggn-it-02512.local>
2020-03-28 16:37:23 +0530

    mongo DB added and get, post, patch, delete request updated

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,11 +1,87 @@
 const express = require('express');
 const routes = express.Router();
+const Order = require('../model/Order')
+const mongoose = require('mongoose');
 
+// Modified Post request using mongoose.
+routes.post('/', (req, res, next) => {
+    const order = new Order({
+        _id : mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    });
+     order.save().then(result => {
+         res.status(201).json({
+             message: "Product POST successfull",
+             order: order
+         })
+     }).catch(error => {
+         res.status(500).json({
+             error: error
+         })
+     });
+});
+
+// Modified GET request using mongoose.
 routes.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: "Product GET Request"
+    Order.find().exec().then(doc => {
+        res.status(200).json({
+            message: "Product GET request",
+            order: doc
+        });
+    }).catch(error => {
+        res.status(500).json({
+            error: error
+        });
     });
 });
+
+// Modified GET request to get the detail of a product using product ID.
+routes.get('/:productId', (req, res, next) => {
+    Order.findById(req.params.productId).exec().then(doc => {
+        res.status(200).json({
+           message: "Data for Given Product ID",
+           order: doc 
+        });
+    }).catch(error => {
+        res.status(500).json({
+            error:  error
+        })
+    });
+})
+
+// Modified DELETE request with Product ID.
+routes.delete('/:productId', (req, res, next) => {
+    Order.remove({_id: req.params.id}).then(result => {
+        res.status(200).json({
+            message: "Order ID deleted",
+            order: result
+        });
+    }).catch(error => {
+        res.status(500).json({
+            error: error
+        })
+    })
+})
+
+// Update(Patch) modified request
+routes.patch('/:productId', (req, res, next) => {
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value
+    }
+    Order.remove({_id: req.params.productId}, {$set: updateOps}).exec().then(result => {
+        res.status(200).json({
+            message: "Product Updated Successfully",
+            order: result
+        })
+    }).catch(error => {
+        res.status(500).json({
+            error: error
+        });
+    });
+})
+
 
 routes.post('/:productId', (req, res, next) => {
     const product = {
--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const productRoutes = require('./api/routes/product')
 const bodyParser = require('body-parser'); // body Parser to get the data from request body
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://premmongodb:mongodb@95@node-rest-shop-nhhf7.mongodb.net/test?retryWrites=true&w=majority", 
+{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+     console.log('connected to database');
+  })
+  .catch(error => {
+     //MongooseServerSelectionError MongooseError [MongooseServerSelectionError]: Authentication failed
+     console.log("Error", error)
+  });
 app.use('/product', productRoutes);
 
 module.exports = app;
